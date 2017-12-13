c0fdc6e5ca2c437a2e67e39a70b36e4f70b45625
Mohammad Adeel <adeel.a.mhd@gmail.com>
2017-12-13 22:37:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,6 +26,7 @@ def won?(board)
   WIN_COMBINATIONS.any? do |win_combination|
     win_combination.all? {|win_index| board[win_index] == 'X'} ||
     win_combination.all? {|win_index| board[win_index] == 'O'}
+    return win_combination
   end
 end
 
