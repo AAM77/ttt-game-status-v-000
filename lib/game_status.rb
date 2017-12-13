c0758c339f0145c5f899d50818b13672030961a8
Mohammad Adeel <adeel.a.mhd@gmail.com>
2017-12-13 22:17:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,29 +22,25 @@ WIN_COMBINATIONS = [
 # Checks if someone has won the game
 def won?(board)
 
-  if board.all? {|position| position == nil || position == "" || position == " "}
-    return false
+  WIN_COMBINATIONS.all? do |win_combination|
 
-  else
-    WIN_COMBINATIONS.select do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+    if (position_1 == 'X' && position_2 == 'X' && position_3 == 'X') || (position_1 == 'O' && position_2 == 'O' && position_3 == 'O')
+      return win_combination
 
-      if (position_1 == 'X' && position_2 == 'X' && position_3 == 'X') || (position_1 == 'O' && position_2 == 'O' && position_3 == 'O')
-        return win_combination
-
-      elsif board.all? {|position| position == 'X' || position == 'O'}
-        return false
-      end
+    elsif board.all? {|position| position == 'X' || position == 'O'}
+      return false
     end
   end
 end
+
 
 #checks if the board is full
 def full?(board)
