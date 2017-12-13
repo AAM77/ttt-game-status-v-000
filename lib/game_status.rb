41c8a868fc672c2bba0d5c0b827bb2beceacd89a
Mohammad Adeel <adeel.a.mhd@gmail.com>
2017-12-13 22:28:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,7 +23,7 @@ WIN_COMBINATIONS = [
 def won?(board)
 
   win_array = []
-  WIN_COMBINATIONS.each do |win_combination|
+  WIN_COMBINATIONS.select do |win_combination|
     win_combination.all? do |win_index|
       position = board[win_index]
 
