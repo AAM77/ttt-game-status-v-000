90190291b718278aa962a02759ff318dd939a261
Mohammad Adeel <adeel.a.mhd@gmail.com>
2017-12-13 22:32:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,8 +25,6 @@ def won?(board)
   win_array = []
   WIN_COMBINATIONS.each do |win_combination|
     win_combination.all? {|win_index| board[win_index] == 'X'}
-      position = board[win_index]
-
       if position = 'X' || position == 'O'
         return win_combination
       else
